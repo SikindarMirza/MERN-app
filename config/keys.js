66415a48bd43138a07a871034f47a224a909d51b
SikindarMirza <sikindar247@gmail.com>
2019-02-17 12:59:40 +0530

    Connected MongoDB

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports ={
+    mongoURI : "mongodb://mern:imfsd247@ds131782.mlab.com:31782/mern"
+}
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// db config
+
+const db = require('./config/keys').mongoURI;
 
 app.get('/', (req, res) => {
     res.send('I am from server...');
 })
 
+mongoose
+    .connect(db)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
