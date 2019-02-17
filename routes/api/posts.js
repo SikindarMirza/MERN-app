e825025399dbf40f6a6b671f2f7d11aa733c91cb
SikindarMirza <sikindar247@gmail.com>
2019-02-17 15:14:59 +0530

    Routes

---
+++ routes/api/posts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/test', (req,res) => {
+    res.json({
+        msg: "Post's api"
+    })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
 
 const app = express();
 
@@ -15,6 +18,11 @@ mongoose
     .connect(db)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
+
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
